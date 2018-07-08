8370ede31c39573d3be2d01282b78f8c09db64cf
Daksha Patel <dakshapatel100@gmail.com>
2018-07-08 16:11:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,12 +18,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, subgenres.name
+  "SELECT DISTINCT name, subgenres.name
   FROM authors
   JOIN series
   ON author.id = series.author_id
-  JOIN series
-  ON subgenre.id = series.subgenre_id LIMIT 2;"
+  JOIN subgenres
+  ON series.subgenre_id = subgenre_id LIMIT 2;"
 end
 
 def select_series_title_with_most_human_characters
